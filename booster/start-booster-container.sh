90648081d08befb968e839066decbe8472600dab
okiwi6 <45100017+oleflb@users.noreply.github.com>
2025-08-17 16:13:48 +0200

    --wip--

--- booster/start-booster-container.sh
+++ booster/start-booster-container.sh
@@ -1,1 +1,11 @@
-podman run -it --rm --name booster-container booster-container:latest bash
+podman run -it --rm \
+  --device /dev/dri \
+  -v $XDG_RUNTIME_DIR:$XDG_RUNTIME_DIR:rw,z \
+  -v subscriber.py:/subscriber.py:Z \
+  -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
+  -e WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
+  -e QT_QPA_PLATFORM=wayland \
+  --security-opt label=type:container_runtime_t \
+  --name booster-container booster-container:latest bash
+
+  # --userns=keep-id \
---
+++ booster/subscriber.py
@@ -0,0 +1,34 @@
+import rclpy
+from rclpy.executors import ExternalShutdownException
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+
+class MinimalSubscriber(Node):
+
+    def __init__(self):
+        super().__init__('minimal_subscriber')
+        self.subscription = self.create_subscription(
+            String,
+            'rt/low_level',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        self.get_logger().info('I heard: "%s"' % msg.data)
+
+
+def main(args=None):
+    try:
+        with rclpy.init(args=args):
+            minimal_subscriber = MinimalSubscriber()
+
+            rclpy.spin(minimal_subscriber)
+    except (KeyboardInterrupt, ExternalShutdownException):
+        pass
+
+
+if __name__ == '__main__':
+    main()
