ba98f5ca14cd0ca3fbf6df9c86a26bea7b928e06
okiwi6 <45100017+oleflb@users.noreply.github.com>
2025-08-20 18:42:24 +0200

    Slimify Containerfile

--- booster/start-booster-container.sh
+++ booster/start-booster-container.sh
@@ -1,11 +1,7 @@
+#!/bin/sh
 podman run -it --rm \
-  --device /dev/dri \
-  -v $XDG_RUNTIME_DIR:$XDG_RUNTIME_DIR:rw,z \
-  -v subscriber.py:/subscriber.py:Z \
-  -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
-  -e WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
-  -e QT_QPA_PLATFORM=wayland \
+  -v /tmp/webots:/tmp/webots/:Z \
+  -e USER=$(whoami) \
+  --device /dev/input \
   --security-opt label=type:container_runtime_t \
-  --name booster-container booster-container:latest bash
-
-  # --userns=keep-id \
+  --name booster-container booster-container:latest
